f1306a1a240ad0b716b0924e04e34a7dfb6db3bd
Marcos Dione <mdione@cloudian.com>
2019-12-13 16:57:18 -0300

    Work around bin/activate behaviour when PS1 is not defined.

--- build-venv.sh
+++ build-venv.sh
@@ -8,5 +8,9 @@ fi
 
 /usr/bin/python2 -m virtualenv --system-site-packages --prompt '(venv: magic-confiblugator) ' .
 
+# there's a 'bug' in bin/activate that references PS1 in situations where it's not defined
+set +u
 source bin/activate
+set -u
+
 pip install -r requirements.txt
