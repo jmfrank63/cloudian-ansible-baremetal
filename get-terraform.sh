3e726873e90c0185a45e6b0c300fb132e4d85f76
Marcos Dione <mdione@cloudian.com>
2019-12-17 13:11:15 -0300

    Use tf 0.11, as we ahdn't migrated to 0.12 yet.

--- get-terraform.sh
+++ get-terraform.sh
@@ -2,8 +2,11 @@
 
 set -eu
 
-wget https://releases.hashicorp.com/terraform/0.12.2/terraform_0.12.2_linux_amd64.zip
-unzip terraform_0.12.2_linux_amd64.zip
+# wget https://releases.hashicorp.com/terraform/0.12.2/terraform_0.12.2_linux_amd64.zip
+# unzip terraform_0.12.2_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.11.15-oci/terraform_0.11.15-oci_linux_amd64.zip
+unzip terraform_0.11.15-oci_linux_amd64.zip
+
 mv terraform bin/
 
 wget https://github.com/sl1pm4t/terraform-provider-lxd/releases/download/v1.2.0/terraform-provider-lxd_v1.2.0_linux_amd64.zip
