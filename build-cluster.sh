1cc8dc2143a4320ef7ff6e882c1490fa7ad44e72
Marcos Dione <mdione@cloudian.com>
2019-12-18 16:52:38 -0300

    Main script.

---
+++ build-cluster.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+set -eu
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 [project_dir] [infra]"
+    exit 1
+fi
+
+project="$1"
+main_file="$project/main.yaml"
+
+if ! [ -f "$main_file" ]; then
+    echo "Clould not find main file '$main_file' in project dir '$project', bailing out!"
+    exit 1
+fi
+
+infra="$2"
+
+if [ -f "$infra" ]; then
+    # it's passing the file with the infra, we need the name, let's see if we can get it
+    infra="${infra#infra/}"  # remove leading infra/
+    infra="${infra%.yaml}"   # remove trailing .yaml
+fi
+
+if ! [ -f "infra/${infra}.yaml" ]; then
+    echo "Cloud not find infra file '$infra', bailing out!"
+    exit 1
+fi
+
+make PROJECT_DIR="$project" INFRA="$infra"
+
+(
+    cd "$project"
+
+    ln -svf ../../.terraform .
+    ln -svf ../../ssh .
+
+    terraform init
+    export ANSIBLE_HOST_KEY_CHECKING=False
+    terraform apply -auto-approve .
+)
