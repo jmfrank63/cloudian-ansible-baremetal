0ec10780ed08c5ac08307a0aa9696db369c530b8
Marcos Dione <mdione@cloudian.com>
2020-01-09 13:05:10 -0300

    Setting some vars make sure we're on the same page everywhere.

--- run-from-orch.sh
+++ run-from-orch.sh
@@ -13,6 +13,15 @@ project="$1"
 hs_version="$2"
 shift 2
 
+# RCs get an RCxx tag in the bin file, but the staging directory and other do not cotain this
+# so have an extra variable with that cleaned up version
+if [[ "$hs_version" ~= "RC[0-9]+" ]]; then
+    # remove the RC tag
+    hs_release_version="${hs_version%RC*}"
+else
+    hs_release_version="${hs_version}"
+fi
+
 # bin/activate references unbound variables
 set +u
 source bin/activate
@@ -20,4 +29,5 @@ set -u
 
 ./bin/ansible-playbook --extra-vars 'run_from_orch=true' --extra-vars 'run_from_iso=false' \
     --extra-vars "project=$project" --extra-vars "hyperstore_version=$hs_version" \
+    --extra-vars "hyperstore_release_version=$hs_release_version" \
     --inventory-file "$project/inventory-fixed.yaml" --verbose deployCluster.yml "$@"
